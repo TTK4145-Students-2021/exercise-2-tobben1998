8b9eed3302fa29c155b37174f316c8021244779d
Tobben <tobias.hagehei@gmail.com>
2021-02-11 11:17:55 +0100

    finished with the go part of task 3

--- 3 - bounded buffer/go/main.go
+++ 3 - bounded buffer/go/main.go
@@ -1,38 +1,39 @@
-
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
+func producer(buffer chan<- int) {
 
-func producer(/*TODO: parameters?*/){
-
-    for i := 0; i < 10; i++ {
-        time.Sleep(100 * time.Millisecond)
-        fmt.Printf("[producer]: pushing %d\n", i)
-        // TODO: push real value to buffer
-    }
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Printf("[producer]: pushing %d\n", i)
+		buffer <- i
+		//DONE TODO: push real value to buffer
+	}
 
 }
 
-func consumer(/*TODO: parameters?*/){
+func consumer(buffer <-chan int) {
 
-    time.Sleep(1 * time.Second)
-    for {
-        i := 0 //TODO: get real value from buffer
-        fmt.Printf("[consumer]: %d\n", i)
-        time.Sleep(50 * time.Millisecond)
-    }
-    
+	time.Sleep(1 * time.Second)
+	for {
+		i := <-buffer //DONE TODO: get real value from buffer
+		fmt.Printf("[consumer]: %d\n", i)
+		time.Sleep(50 * time.Millisecond)
+	}
+
 }
 
+func main() {
 
-func main(){
-    
-    // TODO: make a bounded buffer
-    
-    go consumer()
-    go producer()
-    
-    select {}
+	// Done TODO: make a bounded buffer that can holde 5 elements
+	boundedBuffer := make(chan int, 5)
+
+	go consumer(boundedBuffer)
+	go producer(boundedBuffer)
+
+	select {}
 }
